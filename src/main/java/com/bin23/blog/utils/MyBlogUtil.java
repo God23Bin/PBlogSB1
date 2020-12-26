1ebdbaeb31125830123ae362450c8650c062a751
God23Bin <479017580@qq.com>
2020-12-26 11:46:27 +0800

    1. 博客工具类 2. 实现获取当前的主机地址

---
+++ src/main/java/com/bin23/blog/utils/MyBlogUtil.java
@@ -0,0 +1,15 @@
+package com.bin23.blog.utils;
+
+import java.net.URI;
+
+public class MyBlogUtil {
+    public static URI getHost(URI uri) {
+        URI effectiveURI = null;
+        try {
+            effectiveURI = new URI(uri.getScheme(), uri.getUserInfo(), uri.getHost(), uri.getPort(), null, null, null);
+        } catch (Throwable var4) {
+            effectiveURI = null;
+        }
+        return effectiveURI;
+    }
+}
