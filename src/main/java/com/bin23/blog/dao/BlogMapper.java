89269cc3023fb99eb5365fbb3364ea704501645e
God23Bin <479017580@qq.com>
2020-12-26 11:36:06 +0800

    1. 更新查询方法，按是否发布来查询博客，可区分是在我的文章中还是在草稿箱，替代之前的直接查询全部的 2. 更新了添加博客方法，新增状态，是否发布和是否为垃圾 3. 更新了更新博客方法，新增状态，是否发布和是否为垃圾 4. 新增方法-根据博客标题获取博客id

--- src/main/java/com/bin23/blog/dao/BlogMapper.java
+++ src/main/java/com/bin23/blog/dao/BlogMapper.java
@@ -16,17 +16,44 @@ public interface BlogMapper {
     @Select("SELECT * FROM adm_blog WHERE id = #{id}")
     Blog selectBlogById(Integer id);
 
+    /**
+     * 按是否发布来查询博客，可区分是在我的文章中还是在草稿箱，替代之前的直接查询全部的
+     * @param isPublish
+     * @return
+     */
+    @Select("SELECT * FROM adm_blog WHERE is_publish = #{isPublish}")
+    List<Blog> selectAllBlogByIsPublish(Boolean isPublish);
+
+    /**
+     * 查询已经在垃圾箱中的所有博客
+     * @param isTrash
+     * @return
+     */
+    @Select("SELECT * FROM adm_blog WHERE is_trash = #{isTrash}")
+    List<Blog> selectAllBlogByIsTrash(Boolean isTrash);
+
+    /**
+     * -- 对上面的  查询已经发布的博客进行SQL语句的完善
+     * @param isPublish
+     * @param isTrash
+     * @return
+     */
+    @Select("SELECT * FROM adm_blog WHERE is_publish = #{isPublish} AND is_trash = #{isTrash}")
+    List<Blog> selectAllBlogByIsPublishAndIsTrash(Boolean isPublish, Boolean isTrash);
+
 
     /**
      * 添加博客
+     * 1. 新增状态，是否发布和是否为垃圾
      */
-    @Insert("INSERT INTO adm_blog (title, author, content, create_time, update_time, blog_url, cover_url) VALUES (#{title}, #{author}, #{content}, #{createTime}, #{updateTime}, #{blogUrl}, #{coverUrl})")
+    @Insert("INSERT INTO adm_blog (title, author, content, create_time, update_time, blog_url, cover_url, is_publish, is_trash) VALUES (#{title}, #{author}, #{content}, #{createTime}, #{updateTime}, #{blogUrl}, #{coverUrl}, #{isPublish}, #{isTrash})")
     int insertBlogIntoDB(Blog blog);
 
     /**
      * 修改博客
+     * 1. 新增状态，是否发布和是否为垃圾
      */
-    @Update("UPDATE adm_blog SET title = #{title}, content = #{content}, update_time = #{updateTime}, cover_url = #{coverUrl} WHERE id = #{id}")
+    @Update("UPDATE adm_blog SET title = #{title}, content = #{content}, update_time = #{updateTime}, cover_url = #{coverUrl}, is_publish = #{isPublish}, is_trash = #{isTrash} WHERE id = #{id}")
     int updateBlog(Blog blog);
 
     /**
@@ -34,4 +61,11 @@ public interface BlogMapper {
      */
     @Delete("DELETE FROM adm_blog WHERE id = #{id}")
     int deleteBlogById(Integer id);
+
+    /**
+     * 根据博客标题获取博客id
+     */
+    @Select("SELECT id FROM adm_blog WHERE title = #{title}")
+    int selectIdByBlogTitle(String title);
+
 }
